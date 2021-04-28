079c86359d8bf3dc621c3429dbbddc5bdfb5c1dc
artyomzhibarev <artyom.zhibarev@yandex.ru>
2021-04-28 22:28:15 +0300

    refactor to OOP

--- script.py
+++ script.py
@@ -1,26 +1,51 @@
 import os
 import shutil
-from typing import AnyStr
 import xml.etree.ElementTree as ET
 
 
-def xml_parser(xml_file: AnyStr) -> None:
-    tree = ET.parse(xml_file)
-    config_root = tree.getroot()
-    for file in config_root:
-        file_name = file.attrib['file_name']
-        source_path = os.path.join(file.attrib['source_path'], file_name)
-        destination_path = os.path.join(file.attrib['destination_path'], file_name)
-        path = os.path.dirname(destination_path)
-        if os.path.exists(source_path) and os.path.exists(os.path.dirname(destination_path)):
-            shutil.copyfile(source_path, destination_path)
+class XmlDeserializer:
+    def __init__(self, config):
+        self.config = config
+
+    def deserializer(self):
+        tree = ET.parse(self.config)
+        config_root = tree.getroot()
+        for file in config_root:
+            file_name = file.attrib['file_name']
+            src_path = os.path.join(file.attrib['source_path'], file_name)
+            dst_path = os.path.join(file.attrib['destination_path'], file_name)
+            yield File(file_name, src_path, dst_path)
 
 
+class File:
+    def __init__(self, file_name, src_path, dst_path):
+        self.file_name = file_name
+        if not os.path.isabs(src_path):
+            src_path = os.path.abspath(src_path)
+        if not os.path.isabs(dst_path):
+            dst_path = os.path.abspath(dst_path)
+        self.dst_path = dst_path
+        self.src_path = src_path
 
-# def copy_file(src_, dst_):
-#     shutil.copy(src=src_, dst=dst_)
+
+class FileProvider:
+    @staticmethod
+    def path_validator(file: File) -> bool:
+        return os.path.exists(file.src_path) and os.path.exists(os.path.dirname(file.dst_path))
+
+    @staticmethod
+    def file_copier(file: File) -> None:
+        shutil.copyfile(file.src_path, file.dst_path)
 
 
-if __name__ == '__main__':
-    print(xml_parser('config.xml'))
-    # print(os.path.join('sdf', 'aea'))
+def main(xml_file):
+    xml_des = XmlDeserializer(xml_file)
+    parser = xml_des.deserializer()
+
+    for file in parser:
+        if FileProvider.path_validator(file):
+            FileProvider.file_copier(file)
+
+
+if __name__ == "__main__":
+    main('config.xml')
